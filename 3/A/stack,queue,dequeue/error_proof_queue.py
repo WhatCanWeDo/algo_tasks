7c3a805d5fdb8f2d8e9f0b437704352ac335a3c0
Vladislav <hacer-02@mail.ru>
2026-05-24 18:24:16 +0300

    fixed errors

--- 3/A/stack,queue,dequeue/error_proof_queue.py
+++ 3/A/stack,queue,dequeue/error_proof_queue.py
@@ -1,28 +1,58 @@
-queue = []
+data = [0] * 4
+head = 0
+tail = 0
+count = 0 
+
+def resize():
+    global data, head, tail, count
+    new_data = [0] * (len(data) * 2)
+
+    for i in range(count):
+        new_data[i] = data[(head + i) % len(data)]
+        
+    data = new_data
+    head = 0
+    tail = count
 
 while True:
     command = input().split()
 
     if command[0] == "push":
         number = int(command[1])
-        queue.append(number)
+        
+        if count == len(data):
+            resize()
+
+        data[tail] = number
+        tail = (tail + 1 ) % len(data)
+        count += 1      
         print("ok")
+
     elif command[0] == "pop":
-        if not queue:
+        if count == 0:
             print("error")
         else:
-            value = queue.pop(0)
-            print(value)
+            print(data[head])
+            head = (head + 1 ) % len(data)
+            count -= 1 
+
     elif command[0] == "size":
-        print(len(queue))
+        print(count)
+
     elif command[0] == "front":
-        if not queue:
+        if count == 0 :
             print("error")
         else:
-            print(queue[0])
+            print(data[head])
+
     elif command[0] == "clear":
-        queue.clear()
+        data = [0] * 4
+        head = 0
+        tail = 0 
+        count = 0 
         print("ok")
+
     elif command[0] == "exit":
         print("bye")
         break
+    
